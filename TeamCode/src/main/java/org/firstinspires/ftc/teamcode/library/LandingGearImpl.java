af9b87cbf100b613fa27cf4fd968a6c16689dccc
Luke Reinke <LukeReinke@electrogatorrobotics.com>
2019-04-24 19:54:05 -0500

    more World fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/library/LandingGearImpl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/library/LandingGearImpl.java
@@ -37,9 +37,12 @@ public class LandingGearImpl implements LandingGear {
         drive2.driveServoState(DriveV2.driveServoState.DRIVE);
     }
 
-    public void unhook(){
+    public void unhook() throws InterruptedException {
         drivetrain.slide(4);
+        Thread.sleep(500);
         drivetrain.turn(0);
+        Thread.sleep(500);
+
         deploy();
     }
 
